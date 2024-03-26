8136ba08ddd076a4424b7f223fedd04574563097
duduborges <duduborges333969@gmail.com>
2024-03-26 20:51:28 -0300

    Finalizado

---
+++ script.js
@@ -0,0 +1,40 @@
+/* 
+--------- 1 ----------
+
+let nomes = ["Fernanda", "Arthur", "Theo", "Rafael"]
+
+for (let i = 0; i <= nomes.length; i++) {
+    console.log(nomes[i])
+}
+*/
+
+/* 
+--------- 2 ----------
+
+
+let numeros = [20, 45, 12, 10, 30]
+console.log(numeros.length)
+for (let i = 0; i <= numeros.length; i++) {
+    if (i == 0) {
+        console.log(numeros[i])
+    } else if (i == 5) {
+        console.log(numeros[i])
+    } else {
+        console.log(numeros[i])
+        let soma = numeros[i] + numeros[i - 1]
+        console.log("Soma:" + soma)
+    }
+
+}
+*/
+
+/* 
+--------- 3 ----------
+
+let frutas = ["Banana", "Maça", "Pera"]
+frutas.push("Uva")
+frutas.pop()
+frutas.unshift("Morango")
+frutas.shift()
+console.log(frutas)
+*/
